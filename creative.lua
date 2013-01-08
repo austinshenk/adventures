7662bf4caa7a59abf35c67f80ffb14435857ce3a
Austin Shenk <ashenkster69@gmail.com>
2013-01-07 21:08:31 -0500

    Trying to fix node addition problem
    Problem still isn't fixed when waiting a couple seconds. It seems the
    environment still isn't initalized on player join. But it still adds the
    one node after the other.

--- creative.lua
+++ creative.lua
@@ -5,9 +5,11 @@ minetest.register_on_joinplayer(function(obj)
 	local file = io.open(minetest.get_worldpath().."/adventures_previousmode", "r")
 	if(file:read("*l") ~= adventures.creative) then
 		file:close()
-		for pos,data in pairs(adventures.sourceData) do
-			minetest.env:add_node({name=data[1]}, pos)
-		end
+		minetest.after(2.0, function()
+			for pos,data in pairs(adventures.sourceData) do
+				minetest.env:set_node(pos, {name=data[1]})
+			end
+		end)
 		file = io.open(minetest.get_worldpath().."/adventures_previousmode", "w")
 		file:write(adventures.creative)
 		file:close()
