b0d7f01b249bae73203010c05ca71bad7a7208d4
Austin Shenk <ashenkster69@gmail.com>
2013-01-09 22:34:10 -0500

    Working on removing node from source table

--- encode.lua
+++ encode.lua
@@ -1,15 +1,17 @@
 local file = io.open(minetest.get_worldpath().."/adventures_sources", "w")
 local saved  = false
 local str = ""
-for pos,name in pairs(adventures.sources) do
-	local meta = minetest.env:get_meta(pos)
-	if(name == "adventures:invincible_source") then
-		str = str..name..","..pos.x..","..pos.y..","..pos.z..","..
-		meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-		meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
-		.."\n"
+for str,data in pairs(adventures.sources) do
+	if data ~= nil then 
+		local meta = minetest.env:get_meta(data.pos)
+		if(data.name == "adventures:invincible_source") then
+			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
+			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
+			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			.."\n"
+		end
+		saved = true
 	end
-	saved = true
 end
 file:write(str)
 file:close()
