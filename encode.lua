d964b32077e59f8d41a6055e679e511b2cd4748a
Austin Shenk <ashenkster69@gmail.com>
2013-01-10 21:44:51 -0500

    Fixed writing bug
    local str in for loop was conflicting with str variable used to store
    the string to wirte to file.

--- encode.lua
+++ encode.lua
@@ -1,8 +1,8 @@
 local file = io.open(minetest.get_worldpath().."/adventures_sources", "w")
 local saved  = false
 local str = ""
-for str,data in pairs(adventures.sources) do
-	if data ~= nil then 
+for s,data in pairs(adventures.sources) do
+	if data ~= nil then
 		local meta = minetest.env:get_meta(data.pos)
 		if(data.name == "adventures:invincible_source") then
 			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
