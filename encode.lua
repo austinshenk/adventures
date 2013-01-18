bb60efc3ceb5dc32de4ce42d7dae4eb065d5c0c1
Austin Shenk <ashenkster69@gmail.com>
2013-01-17 20:24:06 -0500

    Added unbuildable, full, spawn, and respawn nodes

--- encode.lua
+++ encode.lua
@@ -4,10 +4,42 @@ local str = ""
 for s,data in pairs(adventures.sources) do
 	if data ~= nil then
 		local meta = minetest.env:get_meta(data.pos)
-		if(data.name == "adventures:invincible_source") then
+		if(data.name == "adventures:unbreakable_source") then
 			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
 			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
 			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			.."\n"
+		end
+		if(data.name == "adventures:unbuildable_source") then
+			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
+			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
+			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			.."\n"
+		end
+		if(data.name == "adventures:fullprotect_source") then
+			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
+			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
+			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			.."\n"
+		end
+		if(data.name == "adventures:spawn_source") then
+			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
+			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
+			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			.."\n"
+		end
+		if(data.name == "adventures:respawn_source") then
+			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
+			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
+			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")..
+			","..meta:get_int("id")
+			.."\n"
+		end
+		if(data.name == "adventures:checkpoint_source") then
+			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
+			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
+			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")..
+			","..meta:get_int("id")
 			.."\n"
 		end
 		saved = true
