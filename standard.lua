45e6ea442146ba651420c0605272d58c1614ccca
Austin Shenk <ashenkster69@gmail.com>
2013-01-17 20:25:43 -0500

    Added spawn functions and other stuff

--- standard.lua
+++ standard.lua
@@ -2,6 +2,7 @@
 
 local old_node_dig = minetest.node_dig
 function minetest.node_dig(pos, node, digger)
+	if (not adventures.canBreak) then return end
 	if adventures.unbreakable[adventures.positionToString(pos)] ~= true then
 		old_node_dig(pos, node, digger)
 	else
@@ -11,13 +12,13 @@ end
 
 local old_node_place = minetest.item_place
 function minetest.item_place(itemstack, placer, pointed_thing)
+	if (not adventures.canBuild) then return end
 	if adventures.unbuildable[adventures.positionToString(pointed_thing.above)] ~= true then
-		return old_node_place(itemstack, placer, pointed_thing)
+		old_node_place(itemstack, placer, pointed_thing)
 	else
 		return
 	end
 end
-
 
 local function storeUnbreakableNodes(data)
 	--local pos = {x=data[2],y=data[3],z=data[4]}
@@ -36,9 +37,6 @@ local function storeUnbreakableNodes(data)
 end
 
 local function storeUnbuildableNodes(data)
-	--local pos = {x=data[2],y=data[3],z=data[4]}
-	--local offset = {x=data[5],y=data[6],z=data[7]}
-	--local size = {width=data[8],length=data[9],height=data[10]}
 	local start = {x=data[2]+data[5]-math.floor(data[8]/2),
 					y=data[3]+data[6]-math.floor(data[10]/2),
 					z=data[4]+data[7]-math.floor(data[8]/2)}
@@ -52,9 +50,6 @@ local function storeUnbuildableNodes(data)
 end
 
 local function storeFullyProtectedNodes(data)
-	--local pos = {x=data[2],y=data[3],z=data[4]}
-	--local offset = {x=data[5],y=data[6],z=data[7]}
-	--local size = {width=data[8],length=data[9],height=data[10]}
 	local start = {x=data[2]+data[5]-math.floor(data[8]/2),
 					y=data[3]+data[6]-math.floor(data[10]/2),
 					z=data[4]+data[7]-math.floor(data[8]/2)}
@@ -68,11 +63,74 @@ local function storeFullyProtectedNodes(data)
 	end
 end
 
+local function storeSpawnPositions(data)
+	local start = {x=data[2]+data[5]-math.floor(data[8]/2),
+					y=data[3]+data[6]-math.floor(data[10]/2),
+					z=data[4]+data[7]-math.floor(data[8]/2)}
+	for y=0,data[10]-1,1 do
+	for z=0,data[8]-1,1 do
+	for x=0,data[8]-1,1 do
+		table.insert(adventures.spawnPoints, {x=start.x+x,y=start.y+y,z=start.z+z})
+		adventures.unbuildable[adventures.positionToString({x=start.x+x,y=start.y+y,z=start.z+z})] = true
+	end
+	end
+	end
+end
+
+local function storeRespawnPositions(data)
+	local start = {x=data[2]+data[5]-math.floor(data[8]/2),
+					y=data[3]+data[6]-math.floor(data[10]/2),
+					z=data[4]+data[7]-math.floor(data[8]/2)}
+	local points = {}
+	for y=0,data[10]-1,1 do
+	for z=0,data[8]-1,1 do
+	for x=0,data[8]-1,1 do
+		table.insert(points, {x=start.x+x,y=start.y+y,z=start.z+z})
+		adventures.unbuildable[adventures.positionToString({x=start.x+x,y=start.y+y,z=start.z+z})] = true
+	end
+	end
+	end
+	adventures.respawnPoints[data[11]] = points
+end
+
+local function storeCheckpointPositions(data)
+	local start = {x=data[2]+data[5]-math.floor(data[8]/2),
+					y=data[3]+data[6]-math.floor(data[10]/2),
+					z=data[4]+data[7]-math.floor(data[8]/2)}
+	for y=0,data[10]-1,1 do
+	for z=0,data[8]-1,1 do
+	for x=0,data[8]-1,1 do
+		adventures.checkPoints[adventures.positionToString({x=start.x+x,y=start.y+y,z=start.z+z})] = data[11]
+	end
+	end
+	end
+end
+
+minetest.register_on_newplayer(function(obj)
+	adventures.playerCheckPoints[obj:get_player_name()] = 0
+	adventures.requestSpawnPosition(obj)
+end)
+
 minetest.register_on_joinplayer(function(obj)
 	if adventures.started then return end
 	for pos,data in pairs(adventures.sourceData) do
-		if(data[1] == "adventures:invincible_source") then
+		if(data[1] == "adventures:unbreakable_source") then
 			storeUnbreakableNodes(data)
+		end
+		if(data[1] == "adventures:unbuildable_source") then
+			storeUnbuildableNodes(data)
+		end
+		if(data[1] == "adventures:fullprotect_source") then
+			storeFullyProtectedNodes(data)
+		end
+		if(data[1] == "adventures:spawn_source") then
+			storeSpawnPositions(data)
+		end
+		if(data[1] == "adventures:respawn_source") then
+			storeRespawnPositions(data)
+		end
+		if(data[1] == "adventures:checkpoint_source") then
+			storeCheckpointPositions(data)
 		end
 	end
 	local file = io.open(minetest.get_worldpath().."/adventures_previousmode", "w")
@@ -82,7 +140,12 @@ minetest.register_on_joinplayer(function(obj)
 end)
 
 minetest.register_abm({
-	nodenames = {"adventures:invincible_source"},
+	nodenames = {"adventures:unbreakable_source",
+				"adventures:unbuildable_source",
+				"adventures:fullprotect_source",
+				"adventures:spawn_source",
+				"adventures:respawn_source",
+				"adventures:checkpoint_source"},
 	interval = 0.5,
     chance = 1,
 	action = function(pos, node, active_object_count, active_object_count_wider)
@@ -94,4 +157,46 @@ minetest.register_abm({
 	end
 })
 
-minetest.register_entity("adventures:invincible_area" ,{})
+minetest.register_entity("adventures:unbreakable_area" ,{})
+minetest.register_entity("adventures:unbuildable_area" ,{})
+minetest.register_entity("adventures:fullprotect_area" ,{})
+minetest.register_entity("adventures:spawn_area" ,{})
+minetest.register_entity("adventures:respawn_area" ,{})
+minetest.register_entity("adventures:checkpoint_area" ,{})
+
+minetest.register_on_respawnplayer(function(obj)
+	local id = adventures.playerCheckPoints[obj:get_player_name()]
+	if id == 0 then
+		adventures.requestSpawnPosition(obj)
+	else
+		adventures.requestRespawnPosition(obj)
+	end
+end)
+
+local function savePlayerID()
+	local file = io.open(minetest.get_worldpath().."/adventures_checkpoints", "w")
+	local str = ""
+	for player, id in pairs(adventures.playerCheckPoints) do
+		str = str..player..","..id.."\n"
+	end
+	file:write(str)
+	file:close()
+end
+
+minetest.register_globalstep(function(dtime)
+	for _,player in pairs(minetest.get_connected_players()) do
+		local id = adventures.checkPoints[adventures.positionToString(adventures.snapPlayerPosition(player:getpos()))]
+		if(id ~= nil) then
+			adventures.playerCheckPoints[player:get_player_name()] = id
+		end
+	end
+	if(not adventures.autoSave) then return end
+	if(adventures.currentTime >= adventures.saveTime) then
+		savePlayerID()
+	end
+	adventures.currentTime = adventures.currentTime+dtime
+end)
+
+minetest.register_on_shutdown(function()
+	savePlayerID()
+end)
