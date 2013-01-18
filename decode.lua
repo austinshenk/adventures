a0a50e85b47df39b44d28910c7b65098300e2d25
Austin Shenk <ashenkster69@gmail.com>
2013-01-17 20:25:07 -0500

    Added checkpoint file decoding
    Fixed bug to now allow string data to be stored alongside the integer
    data in the files

--- decode.lua
+++ decode.lua
@@ -1,17 +1,31 @@
 local file = io.open(minetest.get_worldpath().."/adventures_sources", "r")
 if(file ~= nil) then
-	local start = 1
 	for line in file:lines() do
-		if line == "initialized" or line == "" then
-			return
+		if line ~= "initialized" and line ~= "" then
+			local subdata = line:split(",")
+			local data = {}
+			data[1] = subdata[1]
+			for i=2,table.getn(subdata),1 do
+				local num = tonumber(subdata[i])
+				if(num == 0 and subdata[i] ~= "0") then
+					data[i] = subdata[i]
+				else
+					data[i] = num
+				end
+			end
+			adventures.sourceData[adventures.positionToString({x=data[2],y=data[3],z=data[4]})] = data
 		end
-		local subdata = line:split(",")
-		local data = {}
-		data[1] = subdata[1]
-		for i=2,table.getn(subdata),1 do
-			data[i] = subdata[i]
-		end
-		adventures.sourceData[adventures.positionToString({x=data[2],y=data[3],z=data[4]})] = data
 	end
 end
 file:close()
+
+file = io.open(minetest.get_worldpath().."/adventures_checkpoints", "r")
+
+if(file ~= nil) then
+	for line in file:lines() do
+		if line ~= "initialized" and line ~= "" then
+			local data = line:split(",")
+			adventures.playerCheckPoints[data[1]] = tonumber(data[2])
+		end
+	end
+end
