3c3999383e5885873f1473c402f25b99b9545c73
Austin Shenk <ashenkster69@gmail.com>
2013-01-20 15:57:16 -0500

    Changed comma separator to |
    | is used less in text so it'll prevent some issues when splitting

--- encode.lua
+++ encode.lua
@@ -6,45 +6,48 @@ for s,data in pairs(adventures.sources) do
 	if data ~= nil then
 		local meta = minetest.env:get_meta(data.pos)
 		if(data.name == "adventures:unbreakable_source") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
-			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."|"..
+			meta:get_int("x").."|"..meta:get_int("y").."|"..meta:get_int("z").."|"..
+			meta:get_int("width").."|"..meta:get_int("length").."|"..meta:get_int("height")
 			.."\n"
 		end
 		if(data.name == "adventures:unbuildable_source") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
-			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."|"..
+			meta:get_int("x").."|"..meta:get_int("y").."|"..meta:get_int("z").."|"..
+			meta:get_int("width").."|"..meta:get_int("length").."|"..meta:get_int("height")
 			.."\n"
 		end
 		if(data.name == "adventures:fullprotect_source") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
-			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."|"..
+			meta:get_int("x").."|"..meta:get_int("y").."|"..meta:get_int("z").."|"..
+			meta:get_int("width").."|"..meta:get_int("length").."|"..meta:get_int("height")
 			.."\n"
 		end
 		if(data.name == "adventures:spawn_source") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
-			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."|"..
+			meta:get_int("x").."|"..meta:get_int("y").."|"..meta:get_int("z").."|"..
+			meta:get_int("width").."|"..meta:get_int("length").."|"..meta:get_int("height")
 			.."\n"
 		end
 		if(data.name == "adventures:respawn_source") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
-			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")..
-			","..meta:get_int("id")
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."|"..
+			meta:get_int("x").."|"..meta:get_int("y").."|"..meta:get_int("z").."|"..
+			meta:get_int("width").."|"..meta:get_int("length").."|"..meta:get_int("height")..
+			"|"..meta:get_int("id")
 			.."\n"
 		end
 		if(data.name == "adventures:checkpoint_source") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z..","..
-			meta:get_int("x")..","..meta:get_int("y")..","..meta:get_int("z")..","..
-			meta:get_int("width")..","..meta:get_int("length")..","..meta:get_int("height")..
-			","..meta:get_int("id")
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."|"..
+			meta:get_int("x").."|"..meta:get_int("y").."|"..meta:get_int("z").."|"..
+			meta:get_int("width").."|"..meta:get_int("length").."|"..meta:get_int("height")..
+			"|"..meta:get_int("id")
 			.."\n"
 		end
 		if(data.name == "adventures:initial_stuff") then
-			str = str..data.name..","..data.pos.x..","..data.pos.y..","..data.pos.z.."\n"
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."\n"
+		end
+		if(data.name == "adventures:quest") then
+			str = str..data.name.."|"..data.pos.x.."|"..data.pos.y.."|"..data.pos.z.."\n"
 		end
 		saved = true
 	end
