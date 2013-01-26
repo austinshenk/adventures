fafd5a363f73afaa37f930d07a457d1da8d73924
Austin Shenk <ashenkster69@gmail.com>
2013-01-25 21:32:55 -0500

    Changed the way the story is displayed
    One label wasn't enough to show multiple lines so I made every line
    display through its own label.

--- Book.lua
+++ Book.lua
@@ -1,5 +1,9 @@
 local function showBookFormspec(name, story)
-	return "size[5,9]label[0,0;"..name.."]label[0,1;"..story.."]"
+	local str =  "size[5,9]label[0,0;"..name.."]"
+	for i,line in ipairs(story) do
+		str = str.."label[0,"..((i*0.5)+0.5)..";"..line.."]"
+	end
+	return str
 end
 for name,story in pairs(adventures.registered_books) do
 	local nam = "adventures:book_"..name
