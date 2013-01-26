9b45f27c1da9cedc94ae2d02f694fd2b23c754b1
Austin Shenk <ashenkster69@gmail.com>
2013-01-26 18:49:54 -0500

    Fixed the redundancy of quest system

--- standardQuest.lua
+++ standardQuest.lua
@@ -1,11 +1,11 @@
 local function subdivideObjective(str, desc)
 	local subdata = str:split(" ")
 	if subdata[1] == "Return" then
-		return {command=subdata[1], description=desc, total=subdata[2],content=subdata[3], completed=false, count=0}
+		return {command=subdata[1], description=desc, total=tonumber(subdata[2]),content=subdata[3], completed=false, count=0}
 	elseif subdata[1] == "Collect" then
-		return {command=subdata[1], description=desc, total=subdata[2],content=subdata[3], completed=false, count=0}
+		return {command=subdata[1], description=desc, total=tonumber(subdata[2]),content=subdata[3], completed=false, count=0}
 	elseif subdata[1] == "Kill" then
-		return {command=subdata[1], description=desc, total=subdata[2],content=subdata[3], completed=false, count=0}
+		return {command=subdata[1], description=desc, total=tonumber(subdata[2]),content=subdata[3], completed=false, count=0}
 	end
 end
 local function convertObjectiveString(str, desc)
@@ -23,21 +23,33 @@ local function convertObjectiveString(str, desc)
 	end
 	return objs
 end
+function adventures.updateQuestFormspec(meta, objs)
+	local y = 3+table.getn(objs)
+	local str = "label[3.5,0;"..meta:get_string("name").."]"..
+				"list[context;reward;5,"..(y-3)..";2,2;]"..
+				"list[context;items;1,"..(y-3)..";2,2;]"
+	local quest = adventures.quests[meta:get_string("name")]
+	if quest ~= nil then
+		if not quest.accepted then
+			str = str.."button[3.25,"..(y-2)..";1.5,1;accept;Accept]"
+		elseif quest.completed then
+			str = str.."label[3.25,"..(y-2)..";Completed!]"
+		end
+	end
+	for i,obj in ipairs(objs) do
+		str = str.."label[1,"..(i/2)..";"..obj.description.."]"
+	end
+	y = y+4
+	str = str.."list[current_player;main;0,"..(y-4)..";8,4;]"
+	str = "size[8,"..y.."]"..str
+	return str
+end
 function adventures.storeQuestData(data)
 	local pos = {x=data[2],y=data[3],z=data[4]}
 	local meta = minetest.env:get_meta(pos)
-	local objs = convertObjectiveString(meta:get_string("name"), meta:get_string("objective"), meta:get_string("description"))
-	local y = 3+table.getn(objs)
-	local str = "label[0,0;"..meta:get_string("name").."]"..
-				"list[context;reward;4,"..(y-3)..";2,2;]"..
-				"list[context;items;0,"..(y-3)..";2,2;]"..
-				"button[2.25,"..(y-1)..";1.5,0.75;accept;Accept]"
-	for i,obj in pairs(objs) do
-		str = str.."label[0.125,"..(i/2)..";"..obj.description.."]"
-	end
-	str = "size[6,"..y.."]"..str
-	meta:set_string("formspec", str)
-	adventures.quests[meta:get_string("name")] = {source=pos, objectives = objs, completed=false, active=false}
+	local objs = convertObjectiveString(meta:get_string("objective"), meta:get_string("description"))
+	adventures.quests[meta:get_string("name")] = {source=pos, objectives = objs, accepted=false, completed=false, active=false}
+	meta:set_string("formspec", adventures.updateQuestFormspec(meta, objs))
 end
 
 minetest.register_node("adventures:quest", {
@@ -46,20 +58,20 @@ minetest.register_node("adventures:quest", {
 	groups = {immortal=1},
 	tiles = {"adventures_quest.png"},
 	on_receive_fields = function(pos, formname, fields, sender)
+		local meta = minetest.env:get_meta(pos)
 		if fields.accept then
-			local meta = minetest.env:get_meta(pos)
-			adventures.currentQuests[meta:get_string("name")] = adventures.quests[meta:get_string("name")]
-			local objs = adventures.currentQuests[meta:get_string("name")].objectives
-			for _,obj in pairs(objs) do
+			adventures.quests[meta:get_string("name")].accepted = true
+			local objs = adventures.quests[meta:get_string("name")].objectives
+			for i,obj in ipairs(objs) do
 				if obj.command == "Return" then
-					table.insert(adventures.currentObjectives["Return"], {quest=name, index=i})
+					table.insert(adventures.currentObjectives["Return"], {quest=meta:get_string("name"), index=i})
 				elseif obj.command == "Collect" then
-					print("Storing Collect Objective")
-					table.insert(adventures.currentObjectives["Collect"], {quest=name, index=i})
+					table.insert(adventures.currentObjectives["Collect"], {quest=meta:get_string("name"), index=i})
 				elseif obj.command == "Kill" then
-					table.insert(adventures.currentObjectives["Kill"], {quest=name, index=i})
+					table.insert(adventures.currentObjectives["Kill"], {quest=meta:get_string("name"), index=i})
 				end
 			end
+			meta:set_string("formspec", adventures.updateQuestFormspec(meta, objs))
 		end
 	end,
 	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
@@ -67,5 +79,14 @@ minetest.register_node("adventures:quest", {
 	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
 	return 0 end,
 	allow_metadata_inventory_take = function(pos, listname, index, stack, player)
-	return 0 end
+	if listname == "reward" then
+		local meta = minetest.env:get_meta(pos)
+		local quest = adventures.quests[meta:get_string("name")]
+		if quest == nil then
+			return 0
+		elseif not quest.completed then
+			return 0
+		end
+	end
+	return stack:get_count() end
 })
