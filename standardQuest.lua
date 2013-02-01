e25bdbbc22c4f964d8db3d29ce99e09d0c51703c
Austin Shenk <ashenkster69@gmail.com>
2013-01-31 20:25:07 -0500

    Added start of Return Quest and changed quest formspec

--- standardQuest.lua
+++ standardQuest.lua
@@ -25,28 +25,28 @@ local function convertObjectiveString(str, desc)
 end
 function adventures.updateQuestFormspec(meta, objs)
 	local y = 3+table.getn(objs)
-	local str = "label[3.5,0;"..meta:get_string("name").."]"..
-				"list[context;reward;5,"..(y-3)..";2,2;]"..
-				"list[context;items;1,"..(y-3)..";2,2;]"
+	local str = "label[0,0;"..meta:get_string("name").."]"..
+				"list[context;reward;5.5,"..(y-2.5)..";2,2;]"..
+				"list[context;items;0.5,"..(y-2.5)..";2,2;]"
 	local quest = adventures.quests[meta:get_string("name")]
 	if quest ~= nil then
 		if not quest.accepted then
 			str = str.."button[3.25,"..(y-2)..";1.5,1;accept;Accept]"
 		elseif not quest.completed then
-			str = str.."label[3.25,"..(y-2)..";"..meta:get_string("name").." In Progress.]"
+			str = str.."label[3.25,"..(y-2)..";In Progress.]"
 		elseif quest.completed and not quest.turnedIn then
-			str = str.."label[3.25,"..(y-2)..";"..meta:get_string("name").." Completed!]"..
+			str = str.."label[3.25,"..(y-2)..";Completed!]"..
 						"button[3.25,"..(y-1)..";1.5,1;redeem;Turn In]"
 		elseif quest.turnedIn then
-			--Draw Nothing
+			str = str.."label[3.25,"..(y-2)..";Turned In]"
 		end
 	end
 	drawnReturnBox = false
 	for i,obj in ipairs(objs) do
-		if obj.command == "Return" and not drawnReturnBox then
+		if obj.command == "Return" and not drawnReturnBox and quest.accepted then
 			str = str.."list[context;return;3.5,"..(y-3)..";1,1;]"
 		end
-		str = str.."label[1,"..(i/2)..";"..obj.description.."]"
+		str = str.."label[0.5,"..(i/2)..";"..obj.description.."]"
 	end
 	y = y+4
 	str = str.."list[current_player;main;0,"..(y-4)..";8,4;]"
@@ -59,6 +59,7 @@ function adventures.storeQuestData(data)
 	local objs = convertObjectiveString(meta:get_string("objective"), meta:get_string("description"))
 	adventures.quests[meta:get_string("name")] = {source=pos, objectives = objs, accepted=false, completed=false, turnedIn=false, active=false}
 	meta:set_string("formspec", adventures.updateQuestFormspec(meta, objs))
+	meta:get_inventory():set_size("return", 1)
 end
 
 minetest.register_node("adventures:quest", {
@@ -98,7 +99,38 @@ minetest.register_node("adventures:quest", {
 	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
 	return 0 end,
 	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+		if listname == "return" then
+			return stack:get_count();
+		end
 	return 0 end,
 	allow_metadata_inventory_take = function(pos, listname, index, stack, player)
+		if listname == "return" then
+			return stack:get_count();
+		end
 	return 0 end,
+	on_metadata_inventory_put = function(pos, listname, index, stack, player)
+		local meta = minetest.env:get_meta(pos)
+		if listname == "return" then
+			for _,data in pairs(adventures.currentObjectives["Return"]) do
+				local obj = adventures.quests[data.quest].objectives[data.index]
+				if not obj.completed then
+					if stack:get_name() == obj.content then
+					local n = 0
+					if obj.count+stack:get_count() > obj.total then
+						n = obj.total-obj.count
+						obj.count = obj.total
+					else
+						n = stack:get_count()
+						obj.count = obj.count+n
+					end
+					if obj.count == obj.total then
+						adventures.quests[data.quest].objectives[data.index].completed = true
+					end
+					minetest.env:get_meta(pos):get_inventory():set_stack(listname, index, {name=stack:get_name(), count=stack:get_count()-n})
+					end
+				end
+			end
+		end
+		meta:set_string("formspec", adventures.updateQuestFormspec(meta, adventures.quests[meta:get_string("name")].objectives))
+	end,
 })
